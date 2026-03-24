915eada5df02d16f885a0dcfd69316b339fb2606
Griffin Roach <graffan2005@gmail.com>
2026-03-23 21:26:13 -0400

    Making test to make sure we can push

---
+++ src/test/java/java/RecipeGrabberTest.java
@@ -0,0 +1,24 @@
+package java;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RecipeGrabberTest {
+
+    @Test
+    void main() {
+    }
+
+    @Test
+    void connectToMealDb() {
+    }
+
+    @Test
+    void readJsonAsStringFrom() {
+    }
+
+    @Test
+    void printData() {
+    }
+}
